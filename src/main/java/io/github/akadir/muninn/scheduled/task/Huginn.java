2d1864ca14f04b19008d69ad64e7db8f5882a4b1
kadir <akadirkkoc@gmail.com>
2020-05-09 15:24:43 +0300

    chore: remove auto-generated java-docs

--- src/main/java/io/github/akadir/muninn/scheduled/task/Huginn.java
+++ src/main/java/io/github/akadir/muninn/scheduled/task/Huginn.java
@@ -31,13 +31,6 @@ public class Huginn extends Thread {
     private final FriendService friendService;
     private final TelegramBot telegramBot;
 
-    /**
-     * Allocates a new {@code Thread} object. This constructor has the same
-     * effect as {@linkplain #Thread(ThreadGroup, Runnable, String) Thread}
-     * {@code (null, null, gname)}, where {@code gname} is a newly generated
-     * name. Automatically generated names are of the form
-     * {@code "Thread-"+}<i>n</i>, where <i>n</i> is an integer.
-     */
     public Huginn(AuthenticatedUser user, AuthenticatedUserService authenticatedUserService,
                   FriendService friendService, TelegramBot telegramBot) {
         this.user = user;
@@ -46,18 +39,6 @@ public class Huginn extends Thread {
         this.telegramBot = telegramBot;
     }
 
-    /**
-     * If this thread was constructed using a separate
-     * <code>Runnable</code> run object, then that
-     * <code>Runnable</code> object's <code>run</code> method is called;
-     * otherwise, this method does nothing and returns.
-     * <p>
-     * Subclasses of <code>Thread</code> should override this method.
-     *
-     * @see #start()
-     * @see #stop()
-     * @see #Thread(ThreadGroup, Runnable, String)
-     */
     @Override
     public void run() {
         super.run();
--- src/main/java/io/github/akadir/muninn/scheduled/task/Muninn.java
+++ src/main/java/io/github/akadir/muninn/scheduled/task/Muninn.java
@@ -39,18 +39,7 @@ public class Muninn extends Thread {
         this.friendService = friendService;
         this.changeSetService = changeSetService;
     }
-
-    /**
-     * When an object implementing interface <code>Runnable</code> is used
-     * to create a thread, starting the thread causes the object's
-     * <code>run</code> method to be called in that separately executing
-     * thread.
-     * <p>
-     * The general contract of the method <code>run</code> is that it may
-     * take any action whatsoever.
-     *
-     * @see Thread#run()
-     */
+    
     @Override
     public void run() {
         Twitter twitter = getTwitter();
