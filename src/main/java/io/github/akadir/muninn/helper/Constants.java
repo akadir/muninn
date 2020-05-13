ff64c3fc85e0d10931d0b8351c1a4f0172abc44b
kadir <akadirkkoc@gmail.com>
2020-05-14 01:37:15 +0300

    fix: decrease message character limit for telegram bot

--- src/main/java/io/github/akadir/muninn/helper/Constants.java
+++ src/main/java/io/github/akadir/muninn/helper/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
 
     public static final String OLD_TAG = "<i>Old:</i> ";
     public static final String NEW_TAG = "<i>New:</i> ";
-    public static final int TELEGRAM_MESSAGE_CHARACTER_LIMIT = 8192;
+    public static final int TELEGRAM_MESSAGE_CHARACTER_LIMIT = 3000;
 
     private Constants() {
     }
