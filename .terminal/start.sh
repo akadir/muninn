bd97dbc630d6bb2bfeeb74a8dfd0c4f6527d7258
kadir <akadirkkoc@gmail.com>
2020-05-30 21:46:26 +0300

    doc: update startup.sh

--- .terminal/start.sh
+++ .terminal/start.sh
@@ -1,17 +1,29 @@
 #!/bin/bash
+
+TWITTER_CONSUMER_KEY=""
+TWITTER_CONSUMER_SECRET=""
+TELEGRAM_TOKEN=""
+TELEGRAM_BOT_NAME=""
+MUNINN_RECHECK_PERIOD_IN_HOURS=6
+DATA_SOURCE_URL=""
+DATA_SOURCE_USERNAME=""
+DATA_SOURCE_PASSWORD=""
+
+
 PROGRAM_TYPE="muninnTelegramBot"
 DEPENDENCIES="muninn-1.0.jar:lib/*"
 MAIN_CLASS="io.github.akadir.muninn.TelegramBot"
-PIDS=`ps aux | grep -v grep | grep $PROGRAM_TYPE | awk '{print $2}'`
-if [ -z "$PIDS" ]; then
+PID=`ps aux | grep -v grep | grep $PROGRAM_TYPE | awk '{print $2}'`
+
+if [ -z "$PID" ]; then
   cd "${0%/*}"
-  java -Dtwitter.consumer.key="" -Dtwitter.consumer.secret="" \
-       -Dtelegram.token="" -Dtelegram.bot.name="" \
-       -Dmuninn.recheck.period=1 \
-       -Ddata.source.url="" -Ddata.source.username="" -Ddata.source.password="" \
-       -Duser.timezone=GMT+3 -DprogramType=$PROGRAM_TYPE -cp $DEPENDENCIES $MAIN_CLASS
+  java -Dtwitter.consumer.key=$TWITTER_CONSUMER_KEY -Dtwitter.consumer.secret=$TWITTER_CONSUMER_SECRET \
+       -Dtelegram.token=$TELEGRAM_TOKEN -Dtelegram.bot.name=$TELEGRAM_BOT_NAME \
+       -Dmuninn.recheck.period=$MUNINN_RECHECK_PERIOD_IN_HOURS \
+       -Ddata.source.url=$DATA_SOURCE_URL -Ddata.source.username=$DATA_SOURCE_USERNAME -Ddata.source.password=$DATA_SOURCE_PASSWORD \
+       -DprogramType=$PROGRAM_TYPE -cp $DEPENDENCIES $MAIN_CLASS
 else
-  echo "process running with pid:$PIDS"
+  echo "process running with pid:$PID"
   exit 1
 fi
 exit 0
