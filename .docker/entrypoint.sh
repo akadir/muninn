b1a66f968db2132c0f16abe0586a536565ac35bd
kadir <akadirkkoc@gmail.com>
2020-07-22 21:19:27 +0300

    feat(#31): update docker configuration to generate property file

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,9 +2,14 @@
 classpath="muninn.jar:lib/*"
 mainclass="io.github.akadir.muninn.TelegramBot"
 
-java -Dtwitter.consumer.key=$TWITTER_CONSUMER_KEY -Dtwitter.consumer.secret=$TWITTER_CONSUMER_SECRET \
-     -Dtelegram.token=$TELEGRAM_TOKEN -Dtelegram.bot.name=$TELEGRAM_BOT_NAME \
-     -Dmuninn.recheck.period=$MUNINN_RECHECK_PERIOD_IN_HOURS \
-     -Ddata.source.url=$DATA_SOURCE_URL -Ddata.source.username=$DATA_SOURCE_USERNAME -Ddata.source.password=$DATA_SOURCE_PASSWORD \
-     -Duser.timezone=$USER_TIMEZONE \
-     -cp $classpath $mainclass
+echo "telegram.bot.name=$TELEGRAM_BOT_NAME
+telegram.token=$TELEGRAM_TOKEN
+twitter.consumer.key=$TWITTER_CONSUMER_KEY
+twitter.consumer.secret=$TWITTER_CONSUMER_SECRET
+muninn.recheck.period=$MUNINN_RECHECK_PERIOD_IN_HOURS
+data.source.url=$DATA_SOURCE_URL
+data.source.username=$DATA_SOURCE_USERNAME
+data.source.password=$DATA_SOURCE_PASSWORD
+following.count.limit=$FOLLOWING_COUNT_LIMIT" >> muninn.properties
+
+java -Duser.timezone=$USER_TIMEZONE -cp $classpath $mainclass
